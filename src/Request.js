15f3ee4bc06e791f1fd5fa30db063f17447a5e0a
Nafise Maftoohi <nafise@nafise.local>
2023-03-24 13:33:31 +0000

    fix MOVIE_API_KEY prefix

--- src/Request.js
+++ src/Request.js
@@ -1,5 +1,5 @@
-const key = process.env.MOVIE_API_KEY;
-
+const key = process.env.REACT_APP_MOVIE_API_KEY;
+console.log(process.env.REACT_APP_MOVIE_API_KEY);
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
